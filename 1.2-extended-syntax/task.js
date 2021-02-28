5df0cf9c7032b04bce5cc5000585c133d26bba10
Kiiyas <kiiyas@yandex.ru>
2021-02-28 16:48:39 +0500

    homework completed

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,51 +1,36 @@
 'use strict'
 
 function getResult(a, b, c) {
-    let d = Math.pow(b, 2) - (4 * a * c); // дискриминант
+    const d = Math.pow(b, 2) - (4 * a * c); // дискриминант
     let dSquare = Math.sqrt(d);
     let x = [];
-
-    if (d < 0) {
-        x.push('КОРНЕЙ НЕТ');
-    } else if (d === 0) {
-        x.push(( -Math.pow(b, 2)/ (2 * a)).toFixed(2) );
+    let denom = 2 * a;
+    if (d === 0) {
+        x.push( -b / denom );
     } else if (d > 0) {
-        x.push( (-b + dSquare) / (2 * a).toFixed(2) );
-        x.push( (-b - dSquare) / (2 * a).toFixed(2) );
-    }
-    
+        x.push( (-b + dSquare) / denom );
+        x.push( (-b - dSquare) / denom );
+    }    
     return x;
 }
 
-function getAverageMark(marks){
+function getAverageMark(marks) {
+    let averageMark = 0;
+    if (marks.length == 0) {
+        return averageMark;
+    }
+
+    if (marks.length > 5) {
+        console.log('Оценок больше пяти! Последние оценки (${marks.slice(5)}) убраны из подсчетов.');
+    }
+
     let marksSum = 0;
-    let marksNumber = marks.length;
-    let averageMark;
-    let totalMarks;
-
-    function getMarksAverSum() {
-        for (let i = 0; i < totalMarks.length; i++) {
-            marksSum += totalMarks[i];
-        }
-
-        averageMark = Math.round(marksSum / totalMarks.length);
+    marks = marks.slice(0, 5);
+    for (let i = 0; i < marks.length; i++) {
+        marksSum += marks[i];
     }
-    
+    averageMark = Math.round(marksSum / marks.length);
 
-    if (!marksNumber) {
-        console.log('оценок нет');
-        averageMark == 0;
-    }
-
-    if (marksNumber <= 5) {
-        totalMarks = marks;
-        getMarksAverSum();
-    } else if (marksNumber > 5) {
-        console.log('Оценок больше пяти! Последние оценки (${marks.slice(5)}) убраны из подсчетов.')
-        totalMarks = marks.slice(0, 5);
-        getMarksAverSum();
-    }        
-	
     return averageMark;
 }
 
@@ -61,8 +46,7 @@ function askDrink(name, dateOfBirthday) {
     if (age >= 18) {
         result = `Не желаете ли олд-фэшн, ${name}?`;
     } else {
-        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. 
-    Могу предложить вам замечательный клюквенный компот!`;
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
     }
 
     return result;
