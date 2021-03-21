591f47502fe3c96daaf3e1801e969de39c82ec7c
Kiyas <kiiyas@yandex.ru>
2021-03-21 14:25:28 +0500

    fixed

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -30,34 +30,19 @@ function getAverageMark(marks) {
         marksSum += marks[i];
     }
     averageMark = marksSum / marks.length;
-    roundedAverage = Math.round(averageMark);
+    const roundedAverage = Math.round(averageMark);
 
     return roundedAverage;
 }
 
 
 // 3
-/**
- * 
- * @todo 
- */
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const now = Date.now(new Date());
+    const dateOfBirthday = +new Date(birthday);
+    //console.log(dateOfBirthday);
+    let diff = now - dateOfBirthday;
+    let age = diff / (1000 * 60 * 60 * 24 * 365.25);
+    return (age >= 18);
 }
- /* ex
-
-    let now = new Date();            
-    let age = now.getFullYear() - dateOfBirthday.getFullYear();    
-
-    if (now.getMonth() - dateOfBirthday.getMonth() < 0 && now.getDate() - dateOfBirthday.getDate() < 0) {
-        age -= 1;
-    };
-        
-    if (age >= 18) {
-        result = `Не желаете ли олд-фэшн, ${name}?`;
-    } else {
-        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
-    }
-
-*/
